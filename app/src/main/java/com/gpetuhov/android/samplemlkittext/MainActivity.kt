5f3ba47f13806e1d07b36d5ae0f34331cd8f0680
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-15 10:42:44 +0300

    Add MLKit and images

--- app/src/main/java/com/gpetuhov/android/samplemlkittext/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplemlkittext/MainActivity.kt
@@ -3,6 +3,10 @@ package com.gpetuhov.android.samplemlkittext
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// In this example we will use on-device text recognition.
+// Images with text are located inside the assets folder of the project
+// and are taken from: https://firebase.google.com/docs/ml-kit/recognize-text
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
